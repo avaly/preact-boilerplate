3841af4c70e18b486ef1c4b0f37146955c0d0619
Valentin Agachi <github-com@agachi.name>
2017-03-14 19:28:00 +0000

    Bug: Functional defaultProps are empty when devtools is enabled

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,13 +1,28 @@
 import { h, Component } from 'preact';
 import style from './style';
 
-export default class Home extends Component {
-	render() {
-		return (
-			<div class={style.home}>
-				<h1>Home</h1>
-				<p>This is the Home component.</p>
-			</div>
-		);
-	}
+function Home(props) {
+	return (
+		<div class={style.home}>
+			<h1>Home with default prop: {props.foo}</h1>
+			<p>The prop above is empty with the preact/devtools enabled.</p>
+		</div>
+	);
 }
+
+// class Home extends Component {
+// 	render() {
+// 		return (
+// 			<div class={style.home}>
+// 				<h1>Home with default prop: {this.props.foo}</h1>
+// 				<p>The prop above is correct with the preact/devtools enabled.</p>
+// 			</div>
+// 		);
+// 	}
+// }
+
+Home.defaultProps = {
+	foo: 'bar'
+};
+
+export default Home;
